fdb82fbd189706bfced406f3f04bb8e07eefe01e
daegeseage <daegeseage98@gmail.com>
2022-02-18 00:27:28 +0300

    add_chelou.sh 0.02

--- shell01/ex08/add_chelou.sh
+++ shell01/ex08/add_chelou.sh
@@ -1,7 +1,9 @@
 #!/usr/local/bin/bash
-#sed 's/m/1/g' <<< "$FT_NBR2" | sed 's/r/2/g' | sed 's/d/3/g' | sed 's/o/4/g' | sed 's/c/5/g' #>>> "$FT_NBR3"
-#sed 's/\'/1/g' <<< "$FT_NBR1" | sed 's/\\/2/g' | sed 's/\"/3/g' | sed 's/?/4/g' | sed 's/!/5/g' #>>> "$FT_NBR4" 
-#echo $FT_NBR1 $FT_NBR2 $FT_NBR3 $FT_NBR4
-sed 's/m/1/g' <<< "$FT_NBR2" | sed 's/r/2/g' | sed 's/d/3/g' | sed 's/o/4/g' | sed 's/c/5/g' ;
-sed "s/\'/1/g" <<< "$FT_NBR1" | sed 's/\\/2/g' | sed 's/\"/3/g' | sed 's/?/4/g' | sed 's/!/5/g'
-#echo $FT_NBR3 $FT_NBR4; $(( FT_NBR3 + FT_NBR4 )) 
+export FT_NBR1=\\\'?\"\\\"\'\\
+export FT_NBR2=rcrdmddd
+FT_NBR2=$(sed 's/m/1/g' <<< "$FT_NBR2" | sed 's/r/2/g' | sed 's/d/3/g' | sed 's/o/4/g' | sed 's/c/5/g')
+FT_NBR1=$(sed "s/\'/1/g" <<< "$FT_NBR1" | sed 's/\\/2/g' | sed 's/\"/3/g' | sed 's/?/4/g' | sed 's/!/5/g')
+SUM=$(($FT_NBR1 + $FT_NBR2))
+SUM=$(sed "s/1/g/g" <<< "$SUM"| sed 's/2/t/g' | sed 's/3/a/g' | sed 's/4/i/g' | sed 's/5/o/g' | sed 's/6/ /g')
+echo $FT_NBR1 $FT_NBR2 $SUM
+#$(( FT_NBR3 + FT_NBR4 )) 
