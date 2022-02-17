56e93dd357a4070935acda4f9888541fa9760027
daegeseage <daegeseage98@gmail.com>
2022-02-17 15:05:43 +0300

    add add_chelou.sh (but no cmplited yet)

--- shell01/ex08/add_chelou.sh
+++ shell01/ex08/add_chelou.sh
@@ -1,2 +1,7 @@
-FT_NBR1= \'?"\"'\  echo $FT_NBR1
-FT_NBR2= "rcrdmddd"  echo $FT_NBR2
+#!/usr/local/bin/bash
+#sed 's/m/1/g' <<< "$FT_NBR2" | sed 's/r/2/g' | sed 's/d/3/g' | sed 's/o/4/g' | sed 's/c/5/g' #>>> "$FT_NBR3"
+#sed 's/\'/1/g' <<< "$FT_NBR1" | sed 's/\\/2/g' | sed 's/\"/3/g' | sed 's/?/4/g' | sed 's/!/5/g' #>>> "$FT_NBR4" 
+#echo $FT_NBR1 $FT_NBR2 $FT_NBR3 $FT_NBR4
+sed 's/m/1/g' <<< "$FT_NBR2" | sed 's/r/2/g' | sed 's/d/3/g' | sed 's/o/4/g' | sed 's/c/5/g' ;
+sed "s/\'/1/g" <<< "$FT_NBR1" | sed 's/\\/2/g' | sed 's/\"/3/g' | sed 's/?/4/g' | sed 's/!/5/g'
+#echo $FT_NBR3 $FT_NBR4; $(( FT_NBR3 + FT_NBR4 )) 
